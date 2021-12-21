ea7edb36b592b8df376ff6b76efbfd9a4fdd046a
José Barros Bento de Freitas <josezebento4@gmail.com>
2021-12-20 22:19:47 -0300

    Create Maquina de somar.py

---
+++ Maquina de somar.py
@@ -0,0 +1,9 @@
+print('==========| maquina de somar |==========')
+print('')
+print('(use ponto no lugar da virgula)')
+print('\n\n')
+n1=float (input('insira um número: '))
+n2=float (input('insira um número: '))
+print('a soma de',n1,'e',n2,'é',n1+n2)
+r=n1+n2
+print('a soma de {} e {} é {}'.format(n1,n2,r))
