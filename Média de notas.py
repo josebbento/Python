8a6721069c0d55e9f5427d8094ff15f9f9ad78fa
José Barros Bento de Freitas <josezebento4@gmail.com>
2021-12-21 17:36:51 -0300

    Média de notas V2.0

--- Média de notas.py
+++ Média de notas.py
@@ -1,4 +1,28 @@
-n1=float(input('insira a 1° nota: '))
-n2=float(input('insira a 2° nota: '))
-p=input('quantas casas depois da virgula é usado? ')
-print('a média das notas, {:.{}f} e {:.{}f}, é {:.{}f}'.format(n1,p,n2,p,(n1+n2)/2,p))
+#--------------------------------inputs-----------------------------
+média = input('qual é a média minima? ')
+teste1 = float (input('quanto vale a primeira avaliação? '))
+nota1 = float (input('insira a primeira nota: '))
+teste2 = float (input('quanto vale a segunda avaliação? '))
+nota2 = float (input('insira a segunda nota: '))
+#-----------------------------média com porcentagem---------------
+if '%' in média:
+  média1 = float (média.replace('%',''))
+  média1 = float (média1/100)
+  médiaaluno = (nota1+nota2)/(teste1+teste2)
+#-----------------------média sem porcentagem-----------------------
+else:
+  médiaaluno = float (teste1 + teste2)
+#------------------------------resultados------------------------------
+if médiaaluno < média1 * 60/100:
+  resultado = '\033[31mREPROVADO'
+elif médiaaluno < média1:
+  resultado = '\033[33mRECUPERAÇÃO'
+else:
+  resultado = '\033[32mAPROVADO'
+#-----------------------mostrar a média do aluno-----------------------
+if '%' in média:
+  print('média do aluno: {:.1f}%'.format(médiaaluno*100))
+else:
+  print('média do aluno: {:.1f}'.format(nota1+nota2))
+#-----------------------mostrar a cituação do aluno-----------------------
+print('aluno:',resultado)
