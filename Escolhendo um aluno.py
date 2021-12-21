b7831ad5e407d2ce19de4a0ff2768d1d9a51928d
José Barros Bento de Freitas <josezebento4@gmail.com>
2021-12-21 17:18:48 -0300

    Escolhendo um aluno V 2.0

--- Escolhendo um aluno.py
+++ Escolhendo um aluno.py
@@ -1,8 +1,7 @@
 import random
-a1 = input('o nome do primeiro aluno: ')
-a2 = input('o nome do segundo aluno: ')
-a3 = input('o nome do terceiro aluno: ')
-a4 = input('o nome do terceiro aluno: ')
-alunos=[a1,a2,a3,a4]
-escolhido=random.choice(alunos)
-print(escolhido)
+lista = []
+for p in range (1,5):
+  a = input('o nome do {}° aluno: '.format(p))
+  lista += a
+escolhido=random.choice(lista)
+print('aluno escolhido:',escolhido)
