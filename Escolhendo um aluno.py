20f95cf3bf08538ab6b4e2757503eb36dc9a9a80
José Barros Bento de Freitas <josezebento4@gmail.com>
2021-12-21 17:16:54 -0300

    Create Escolhendo um aluno.py

---
+++ Escolhendo um aluno.py
@@ -0,0 +1,8 @@
+import random
+a1 = input('o nome do primeiro aluno: ')
+a2 = input('o nome do segundo aluno: ')
+a3 = input('o nome do terceiro aluno: ')
+a4 = input('o nome do terceiro aluno: ')
+alunos=[a1,a2,a3,a4]
+escolhido=random.choice(alunos)
+print(escolhido)
