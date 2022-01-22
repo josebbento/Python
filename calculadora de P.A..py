33e4c5d0b7327894e623184ba991e136ab0ba122
José Barros Bento de Freitas <josezebento4@gmail.com>
2022-01-22 19:05:15 -0300

    Calculadora de P.A. V 1.1

--- calculadora de P.A..py
+++ calculadora de P.A..py
@@ -1,5 +1,5 @@
 print('''|===========================|
-| Calculadora de P.A. V 1.0 |
+| Calculadora de P.A. V 1.1 |
 |===========================|
 
 ''')
@@ -7,18 +7,33 @@ sair = ''
 
 while sair != 's' and sair != 'sim' and sair != 'com certeza' and sair != 'claro':
 
-  contador = 0
+    contador = 0
+    quantos = ''
 
-  termo1 = float (input ('insira o primeiro termo da P.A.: '))
+    termo1 = float (input ('insira o primeiro termo da P.A.: '))
 
-  razão = float (input ('insira a razão da P.A.: '))
+    razão = float (input ('insira a razão da P.A.: '))
 
-  print('P.A. de {}: '.format(termo1),end='')
+    print('P.A. de {}: '.format(termo1),end='')
 
-  while contador != 9:
-    print('{}, '.format(termo1 + razão * contador),end = '')
-    contador += 1
-  print(termo1 + razão * contador)
-  print()
+    while contador != 9:
+        print('{}, '.format(termo1 + razão * contador),end = '')
+        contador += 1
+    print(termo1 + razão * contador)
+    print()
 
-  sair = input ('deseja sair? ')
+    while quantos != 0 :
+        quantos = float (input ('insira a quantidade de termos a mais que gostaria de ver: '))
+        
+        if quantos != 0:
+          quantos += contador
+          while contador < quantos-1:
+            contador += 1
+            print('{}, '.format(termo1 + razão * contador),end = '') 
+          contador += 1
+          print(termo1 + razão * contador)
+          
+        print()
+
+    sair = input('deseja sair? ')
+    print('\n')
