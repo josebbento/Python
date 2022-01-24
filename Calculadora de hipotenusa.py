f62e3bc928b8604538d360606e28a60571da7d19
José Barros Bento de Freitas <josezebento4@gmail.com>
2022-01-24 00:41:26 -0300

    Create Calculadora de hipotenusa.py

---
+++ Calculadora de hipotenusa.py
@@ -0,0 +1,4 @@
+n1=float(input('insira um dos catetos: '))
+n2=float(input('insira o outo cateto: '))
+n3=(n1 ** 2 + n2 ** 2)**(1/2)
+print('a hipotenusa de um triangulo com {} por {} é de {}.'.format(n1,n2,n3))
