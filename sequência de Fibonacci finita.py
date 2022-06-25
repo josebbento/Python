fdbb022207c6b51742cd4e6b695c0b25c284db8c
José Barros Bento de Freitas <josezebento4@gmail.com>
2022-06-25 20:06:17 -0300

    Sequência de fibonacci V 1.0.1

--- sequência de Fibonacci finita.py
+++ sequência de Fibonacci finita.py
@@ -9,24 +9,40 @@ while sair != 's' and sair != 'sim':
   c2 = float(input('insira quantos números da sequência de Fibonacci: '))
 
   while c1 < c2:
-    
-    n2 = n3 + n1
-    print(n2)
+    if c1 < c2-1:
+      n2 = n3 + n1
+      print(n2,end=', ')
+      c1 += 1
 
-    c1 += 1
-
-    if n1 == 0 :
+    if n1 == 0:
       n1=1
 
-    if c1 < c2:   
+    if c1 < c2-1:
       n3 = n1 + n2
-      print(n3)
+      print(n3,end=', ')
       c1 += 1
 
-    if c1 < c2:
+    if c1 < c2-1:
       n1 = n2 + n3
-      print(n1)
+      print(n1,end=', ')
       c1 += 1
 
-  sair = input('deseja sair do programa? ').lower()
+    if (c2 % 3 == 0):
+      if c1 == c2-1:
+        n1 = n2 + n3
+        print(n1)
+        c1 += 1
+    if (c2 % 3 == 1):
+      if c1 == c2-1:
+        if c2 != 1:
+          n2 = n3 + n1
+        print(n2)
+        c1 += 1
+    if (c2 % 3 == 2):
+      if c1 == c2-1:
+        n3 = n1 + n2
+        print(n3)
+        c1 += 1
+
+  sair = input('\ndeseja sair do programa? ').lower()
   print()
