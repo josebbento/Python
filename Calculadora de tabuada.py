9346d2117c0769a32c3862f4984751f4aa6ab964
José Barros Bento de Freitas <josezebento4@gmail.com>
2022-07-24 15:10:03 -0300

    Update Calculadora de tabuada.py

--- Calculadora de tabuada.py
+++ Calculadora de tabuada.py
@@ -1,6 +1,8 @@
 n = 0
 while n >= 0:
     n = float(input('Insira um número (maior que 0): '))
+    if n < 0:
+        break
     carac_n = len(f'Insira um número (maior que 0): {n}')
     carac_t = len(f'{n} X 10 = {n*10}')-2
     print('=' * carac_n if carac_n > carac_t else '='*carac_t)
