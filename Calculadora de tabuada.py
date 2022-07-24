b0ab1a9f7d21084cce0d3bbd84e24d0856b88e83
José Barros Bento de Freitas <josezebento4@gmail.com>
2022-07-24 13:24:32 -0300

    Create Calculadora de tabuada.py

---
+++ Calculadora de tabuada.py
@@ -0,0 +1,13 @@
+n = 0
+while n >= 0:
+    n = float(input('Insira um número (maior que 0): '))
+    carac_n = len(f'Insira um número (maior que 0): {n}')
+    print(carac_n)
+    carac_t = len(f'{n} X 10 = {n*10}')-2
+    print(carac_t)
+    print('=' * carac_n if carac_n > carac_t else '='*carac_t)
+
+    for m in range(1,11):
+        print(f'{n} X {m} = {n*m}')
+
+    print('=' * carac_n if carac_n > carac_t else '='*carac_t)
